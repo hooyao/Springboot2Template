027dcfef8d001ee6026dc54e8995ea4454797776
Yao Hu <huy@tradeshift.com>
2017-12-03 11:16:46 +0800

    init

---
+++ src/main/java/com/hy/springboot2poke/Springboot2pokeApplication.java
@@ -0,0 +1,12 @@
+package com.hy.springboot2poke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2pokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2pokeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hy/springboot2poke/Springboot2pokeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hy.springboot2poke;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot2pokeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
